ffbfc05f681efc2c37b3a74a34e717a8b6a6fef0
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-22 19:03:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
-def greet_characters(array)
-  arrays.each {|character|
-puts "Hello #{character}!"}
+#
+  def greet_characters(array)
+      array.each { |i|
+          puts "Hello #{i}!" }
   end
-
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
